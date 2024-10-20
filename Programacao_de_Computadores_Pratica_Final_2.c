5c8b5e3a98678710c0364e4054f5966751f8edaf
KevynFelipe <137632036+KevynFelipe@users.noreply.github.com>
2024-10-20 15:17:27 -0300

    initial

--- Programacao_de_Computadores_Pratica_Final_2.c
+++ Programacao_de_Computadores_Pratica_Final_2.c
@@ -1,24 +1,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    int temperaturas[100];
-    char temperatura_recebida[100];
-    // FILE *fp;
-    FILE *fptr;
-    // fp=fopen ("temperaturas.txt", "a");
-    // fprintf(fp,"%i\n", temperatura);
-    // fclose(fp);
-fptr=fopen ("temperaturas.txt", "r");
-if(fptr){
-        while(fgets(temperatura_recebida, sizeof(temperatura_recebida), fptr) != NULL)
+    char temperatura_recebida[10];
+    float soma = 0;
+    int contador = 0;
+    float media;
+    char char_media[20];
+    FILE *temperaturas;
+    FILE *temperatura_media;
+
+    temperaturas=fopen ("temperaturas.txt", "r");
+    if (temperaturas == NULL) { printf("Não foi possível abrir o arquivo"); }
+        while(fgets(temperatura_recebida, sizeof(temperatura_recebida), temperaturas) != NULL)
         {
-        printf("Teste: %s\n", temperatura_recebida);
-        fscanf(fptr, "%d", &temperaturas);
-        printf("%i", temperaturas);
+        soma = soma + atof(temperatura_recebida);
+        contador++;
+        printf("Temperatura de: %s Graus Celsius\n", temperatura_recebida);
         };
-    fclose(fptr);
-}
+    fclose(temperaturas);
+    
+    media = soma / contador;
+    ltoa(media, char_media , 10);
+    temperatura_media = fopen("temperatura_media", "w");
+    if (temperatura_media == NULL) { printf("Não foi possível abrir o arquivo"); }
+    fprintf(temperatura_media, char_media);
+
+printf("Soma das temperaturas: %.2f\n", soma);
+printf("Media das temperaturas: %.2f\n", media);
+printf("Media das temperaturas: %s\n", char_media);
 return 1;
 }
