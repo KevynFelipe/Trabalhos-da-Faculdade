004e8808d7a490083818ddd90d7969540fd4ac5d
KevynFelipe <137632036+KevynFelipe@users.noreply.github.com>
2024-10-21 09:15:08 -0300

    initial

--- Programacao_de_Computadores_Pratica_Final_1.c
+++ Programacao_de_Computadores_Pratica_Final_1.c
@@ -1,6 +1,5 @@
 #include <ctype.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -8,14 +7,14 @@ int verificar_telefone(char numero_de_celular[12]){
     size_t i = 0;
     for (i = 0; i < strlen(numero_de_celular); i++){
     if (isdigit(numero_de_celular[i])){
-        printf("%c e um numero!\n", numero_de_celular[i]);}
+        printf("%c é um número!\n", numero_de_celular[i]);}
     else {
-    printf("%c nao e um numero!\n", numero_de_celular[i]);
+    printf("%c não é um número!\n", numero_de_celular[i]);
     return 0;
     };
 
     };
-    printf("\nO numero de telefone: %s possui %zu digitos\n", numero_de_celular,strlen(numero_de_celular));
+    printf("\nO número de telefone: %s possui %zu digitos\n", numero_de_celular,strlen(numero_de_celular));
     return 1;
 };
 
